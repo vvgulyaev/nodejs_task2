da1ed0a95da6f5d1b8b4c1016c4b810f059a4861
Vyacheslav Gulyaev <vv_gulyaev@mail.ru>
2016-11-25 21:32:20 +0300

    add task 2X

--- src/index.js
+++ src/index.js
@@ -9,6 +9,77 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/task2X', (req, res) => {
+
+	var request = (+req.query.i || 0);
+	var result = 0;
+
+	switch(request) 
+	{
+	    case 0:
+			result = 1;
+	        break;
+	    case 1:
+			result = 18;
+	        break;
+	    case 2:
+			result = 243;
+	        break;
+	    case 3:
+			result = 3240;
+	        break;
+	    case 4:
+			result = 43254;
+	        break;
+	    case 5:
+			result = 577368;
+	        break;
+	    case 6:
+			result = 7706988;
+	        break;
+	    case 7:
+			result = 102876480;
+	        break;
+	    case 8:
+			result = 1373243544;
+	        break;
+	    case 9:
+			result = 18330699168;
+	        break;
+	    case 10:
+			result = 244686773808;
+	        break;
+	    case 11:
+			result = 3266193870720;
+	        break;
+	    case 12:
+			result = 43598688377184;
+	        break;
+	    case 13:
+			result = 581975750199168;
+	        break;
+	    case 14:
+			result = 7768485393179328;
+	        break;
+	    case 15:
+			result = 103697388221736960;
+	        break;
+	    case 16:
+			result = 1384201395738071424;
+	        break;
+	    case 17:
+			result = 18476969736848122368;
+	        break;
+	    case 18:
+			result = 246639261965462754048;
+	        break;
+	    default:
+			result = 'not yet implemented';
+	}
+
+	res.send(result.toString());	
+});
+
 
 app.get('/task2A', (req, res) => {
 	const sum = (+req.query.a || 0) + (+req.query.b || 0);
